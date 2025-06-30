5e78492dbb0811323cb4e95178292b83bafe8331
upsaurav12 <sauravup041103@gmail.com>
2025-06-30 19:30:07 +0530

    backend: cache: added more unit tests

--- backend/pkg/k8cache/k8cache_test.go
+++ backend/pkg/k8cache/k8cache_test.go
@@ -16,14 +16,90 @@ package k8cache_test
 import (
 	"bytes"
 	"compress/gzip"
+	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"reflect"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/kubernetes-sigs/headlamp/backend/pkg/cache"
 	"github.com/kubernetes-sigs/headlamp/backend/pkg/k8cache"
+	"github.com/kubernetes-sigs/headlamp/backend/pkg/kubeconfig"
 	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
+
+// MockCache is struct which help to mock caching for testing purpose.
+type MockCache struct {
+	mu    sync.RWMutex
+	store map[string]string
+	err   error
+}
+
+// Helps to initialize cache struct for tests.
+func NewMockCache() *MockCache {
+	return &MockCache{
+		store: make(map[string]string),
+	}
+}
+
+// Mocks storing of value with its corresponding key string.
+func (m *MockCache) Set(ctx context.Context, key, value string) error {
+	if m.err != nil {
+		return m.err
+	}
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.store[key] = value
+
+	return nil
+}
+
+// Mocks storing of value with its corresponding key string with time-to-live.
+func (m *MockCache) SetWithTTL(ctx context.Context, key, value string, ttl time.Duration) error {
+	return m.Set(ctx, key, value)
+}
+
+// Mocks deleting value with the help of key string.
+func (m *MockCache) Delete(ctx context.Context, key string) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	delete(m.store, key)
+
+	return nil
+}
+
+// Mocks retrieval of value with its corresponding key string.
+func (m *MockCache) Get(ctx context.Context, key string) (string, error) {
+	if m.err != nil {
+		return "", m.err
+	}
+
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	val, ok := m.store[key]
+
+	if !ok {
+		return "", errors.New("not found")
+	}
+
+	return val, nil
+}
+
+// Mocks retrieving all the values inside the cache.
+func (m *MockCache) GetAll(ctx context.Context, selectFunc cache.Matcher) (map[string]string, error) {
+	return nil, nil
+}
+
+// Mocks updating of time-to-live with the helo of its corresponding key string.
+func (m *MockCache) UpdateTTL(ctx context.Context, key string, ttl time.Duration) error {
+	return nil
+}
 
 // TestInitialize verifies that responseCapture is initialized with
 // the original http.ResponseWriter and an empty buffer.
@@ -141,6 +217,166 @@ func TestGetResponseBody(t *testing.T) {
 	}
 }
 
+// TestUnMarshallCacheData tests whether the resource Data unserialized correctly.
+// It contains different test cases where the inputs empty , valid and invalid.
+func TestUnMarshallCacheData(t *testing.T) {
+	tests := []struct {
+		name                   string
+		cacheResource          string
+		cacheData              k8cache.CachedResponseData
+		expectedCachedResponse k8cache.CachedResponseData
+		expectedError          error
+	}{
+		{
+			name:          "cache Resource is valid",
+			cacheResource: `{"key": "1234" , "body":"testing-data"}`,
+			cacheData:     k8cache.CachedResponseData{},
+			expectedCachedResponse: k8cache.CachedResponseData{
+				Body: "testing-data",
+			},
+			expectedError: nil,
+		},
+		{
+			name:                   "cache Resource input is valid but cacheResponse is empty",
+			cacheResource:          `{"key" :"1234" , "value": "testing-data"}`,
+			cacheData:              k8cache.CachedResponseData{},
+			expectedCachedResponse: k8cache.CachedResponseData{},
+			expectedError:          nil,
+		},
+		{
+			name:                   "cache Resource is invalid",
+			cacheResource:          "testing-string",
+			cacheData:              k8cache.CachedResponseData{},
+			expectedCachedResponse: k8cache.CachedResponseData{},
+			expectedError:          errors.New("invalid character 'e' in literal true (expecting 'r')"),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := k8cache.UnmarshalCachedata(tc.cacheResource, tc.cacheData)
+			assert.Equal(t, tc.expectedCachedResponse, result)
+
+			if err != nil {
+				assert.ErrorContains(t, err, tc.expectedError.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+// TestSetHeader tests whether the SetHeader is providing correct metadata for
+// the given cacheData that will going to be served to the client.
+func TestSetHeader(t *testing.T) {
+	tests := []struct {
+		name              string
+		cacheData         k8cache.CachedResponseData
+		expectedCacheData k8cache.CachedResponseData
+	}{
+		{
+			name: "cache data is valid",
+			cacheData: k8cache.CachedResponseData{
+				StatusCode: 200,
+				Headers: http.Header{
+					"Content-Type": {"application/json"},
+					"X-Test":       {"true"},
+				},
+				Body: `{"message": "OK"}`,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			k8cache.SetHeader(tc.cacheData, rr)
+
+			for key, expectedValue := range tc.cacheData.Headers {
+				actualValues := rr.Header().Values(key)
+				if !reflect.DeepEqual(actualValues, expectedValue) {
+					t.Errorf("Header %s: expected %v, got %v", key, expectedValue, actualValues)
+				}
+			}
+		})
+	}
+}
+
+// TestMarshallToStore tests whether the MarshallToStore
+// serialized correctly that will be stored into the cache.
+func TestMarshallToStore(t *testing.T) {
+	tests := []struct {
+		name          string
+		cacheData     k8cache.CachedResponseData
+		expectedData  string
+		expectedError error
+	}{
+		{
+			name: "cache data is valid",
+			cacheData: k8cache.CachedResponseData{
+				StatusCode: 200,
+				Headers: http.Header{
+					"Context-Type": {"application/json"},
+					"X-Test":       {"true"},
+				},
+				Body: "test-body",
+			},
+			expectedData: `{"statusCode":200,"headers":{"Context-Type":["application/json"],"X-Test":["true"]},` +
+				`"body":"test-body"}`,
+
+			expectedError: nil,
+		},
+
+		{
+			name:          "cache data is invalid",
+			cacheData:     k8cache.CachedResponseData{},
+			expectedData:  "{\"statusCode\":0,\"headers\":null,\"body\":\"\"}",
+			expectedError: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			data, err := k8cache.MarshallToStore(tc.cacheData)
+			assert.Equal(t, tc.expectedData, string(data))
+			assert.NoError(t, err)
+		})
+	}
+}
+
+// TestSetHeaderToCache test whether the extracted header while capturing
+// adding up headers that will going to store in the cache with their corresponding
+// response body.
+func TestSetHeaderToCache(t *testing.T) {
+	tests := []struct {
+		name           string
+		responseHeader http.Header
+		encoding       string
+		expectedHeader http.Header
+	}{
+		{
+			name: "headers are valid",
+			responseHeader: http.Header{
+				"Content-Type":     {"application/json"},
+				"Content-Encoding": {"gzip"},
+				"X-Test":           {"test"},
+			},
+			encoding: "gzip",
+			expectedHeader: http.Header{
+				"Content-Type": {"application/json"},
+				"X-Test":       {"test"},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			header := k8cache.SetHeadersTocache(tc.responseHeader, tc.encoding)
+			assert.Equal(t, tc.expectedHeader, header)
+		})
+	}
+}
+
 // TestGenerateKey ensures the generated key is valid for both normal
 // and empty cluster name scenarios.
 func TestGenerateKey(t *testing.T) {
@@ -157,4 +393,104 @@ func TestGenerateKey(t *testing.T) {
 		assert.NoError(t, err)
 		assert.NotEmpty(t, key)
 	})
+}
+
+// TestIsAllowed test whether the function is returning correct boolean value
+// while making SSAR request.
+func TestIsAllowed(t *testing.T) {
+	tests := []struct {
+		name      string
+		urlObj    *url.URL
+		kContext  kubeconfig.Context
+		isAllowed bool
+	}{
+		{
+			name:   "user is allowed",
+			urlObj: &url.URL{Path: "/apis/authorization.k8s.io/v1/selfsubjectrulesreviews"},
+			kContext: kubeconfig.Context{
+				Name:    "hello-world",
+				Cluster: &api.Cluster{},
+			},
+			isAllowed: true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			rw := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, tc.urlObj.Path, nil)
+
+			isAllowed := k8cache.IsAllowed(tc.urlObj, &tc.kContext, rw, r)
+			assert.Equal(t, tc.isAllowed, isAllowed)
+		})
+	}
+}
+
+// TestLoadFromCache tests whether the cache data is being served to the
+// client correctly.
+func TestLoadfromCache(t *testing.T) {
+	tests := []struct {
+		name          string
+		key           string
+		isLoaded      bool
+		value         string
+		expectedError error
+	}{
+		{
+			name:          "Served from cache",
+			key:           "test-key",
+			value:         `{"Body":"from_cache","StatusCode":200}`,
+			isLoaded:      true,
+			expectedError: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			mockCache := NewMockCache()
+
+			err := mockCache.SetWithTTL(context.Background(), tc.key, tc.value, 0)
+
+			assert.NoError(t, err)
+
+			w := httptest.NewRecorder()
+
+			isLoaded, err := k8cache.LoadfromCache(mockCache, tc.isLoaded, tc.key, w)
+			assert.Equal(t, tc.isLoaded, isLoaded)
+			assert.NoError(t, err)
+		})
+	}
+}
+
+// TestRequestToCache tests whether the cache storing the response data.
+func TestRequestToK8AndStore(t *testing.T) {
+	tests := []struct {
+		name          string
+		urlObj        *url.URL
+		kContext      kubeconfig.Context
+		key           string
+		expectedError error
+	}{
+		{
+			name:   "valid workflow",
+			urlObj: &url.URL{Path: "/api/v1/pods"},
+			kContext: kubeconfig.Context{
+				Name:    "hello-world",
+				Cluster: &api.Cluster{},
+			},
+			key:           "1234",
+			expectedError: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			rw := httptest.NewRecorder()
+			rcw := k8cache.Initialize(rw)
+			r := httptest.NewRequest(http.MethodGet, tc.urlObj.Path, nil)
+			newCache := NewMockCache()
+			err := k8cache.RequestToK8sAndStore(newCache, &tc.kContext, tc.urlObj, rcw, r, tc.key)
+			assert.NoError(t, err)
+		})
+	}
 }
